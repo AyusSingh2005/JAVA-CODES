156aa0329e788f0e67ed9e83103116af461f35b6
Ayush Singh <awsmsi0405@gmail.com>
2025-10-15 10:30:07 +0530

    OTP GENERATER

---
+++ src/Projects/OTPGENERATER.java
@@ -0,0 +1,14 @@
+//package Projects;
+//import java.security.SecureRandom;
+//public class OTPGENERATER {
+//    private static final SecureRandom
+//    secureRandom = new SecureRandom();
+//
+//    public static String generateOTP(){
+//        int number = secureRandom.nextInt(10000);
+//        return String.format("%04d",number);
+//    }
+//    public static void main(String[]args){
+//        System.out.println("YOUR OTP IS: "+generateOTP());
+//    }
+//}
