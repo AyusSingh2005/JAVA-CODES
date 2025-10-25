17f02c49f94714113a5fc22ea84fd44f412c5f1d
Ayush Singh <awsmsi0405@gmail.com>
2025-10-25 06:23:13 +0530

    updated

--- src/LinkedList/LinkedListPractice.java
+++ src/LinkedList/LinkedListPractice.java
@@ -9,6 +9,8 @@ class Data{
     }
 }
 
+
+
 public class LinkedListPractice {
     Scanner sc = new Scanner(System.in);
     Data head;
@@ -23,6 +25,8 @@ public class LinkedListPractice {
            temp.next=newData;
        }
     }
+
+
 
     public void Display(){
         if(head==null) System.out.println("the list is empty!");
@@ -40,46 +44,31 @@ public class LinkedListPractice {
     public void delete(){
         System.out.print("enter element you want to delete: ");
         int target=sc.nextInt();
+        Data temp=head,prev=null;
         if(head==null){
             System.out.println("Empty List!");
         }
-        else{
-            Data temp=head;
-            while(temp.next!=null){
-                temp=temp.next;
-                if(temp.data==target){
-                    temp.data=temp.next.data;
-                    System.out.println("Done!");return;}
-
-
-            }
+       else if(head.data==target) System.out.println("ElementDeleted");
+       else{
+           while(temp.next!=null && temp.data!=target){
+               prev=temp;
+               temp=temp.next;
+           }
+           prev.next=temp.next;
         }
+
+
     }
 
-    public void sorting(){
-        if(head==null) System.out.println("Empty List");
-        else{
-            Data temp=head.next, prev = head;
-            while(temp!=null){
-                //20 50 30 40 60
-                 if(temp.data<prev.data){
-                     Data New = temp;
-                     temp=prev;
-                     prev=New;
-                 }
-                temp=temp.next;
-            }
-            System.out.println("Sorting Done");
-        }
-        Display();
-    }
+
 
 
     public static void main(String[]args){
         LinkedListPractice list = new LinkedListPractice();
         list.add(20);list.add(50);list.add(30);list.add(40);list.add(60);
         list.Display();
-        //list.delete();
-    list.sorting();
+        list.delete();
+        list.Display();
+
     }
 }
--- src/contactManagementWithDB/DBConnection.java
+++ src/contactManagementWithDB/DBConnection.java
@@ -4,7 +4,7 @@ import java.sql.DriverManager;
 public class DBConnection {
     private static final String URL = "jdbc:mysql://localhost:3306/ContactManager";
     private static final String USER = "root";
-    private static final String PASSWORD = "********"; ////ACTUAL PASSWORD HERE
+    private static final String PASSWORD = "*****8"; ////ACTUAL PASSWORD HERE
     public static Connection getConnection(){
         try{
             Class.forName("com.mysql.cj.jdbc.Driver");
