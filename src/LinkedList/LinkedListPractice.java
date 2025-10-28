1adbb7d4c769ab0282c34f63a72216298ea7c36a
Ayush Singh <awsmsi0405@gmail.com>
2025-10-29 03:18:48 +0530

    linkedListUpdates

--- src/LinkedList/LinkedListPractice.java
+++ src/LinkedList/LinkedListPractice.java
@@ -14,9 +14,11 @@ class Data{
 public class LinkedListPractice {
     Scanner sc = new Scanner(System.in);
     Data head;
+    int count=0;
 
     public void add(int data){
        Data newData = new  Data(data);
+
        if(head==null)head = newData;
        else{
            Data temp=head;
@@ -24,6 +26,7 @@ public class LinkedListPractice {
                temp=temp.next;
            }
            temp.next=newData;
+           count++;
        }
     }
 
@@ -31,6 +34,7 @@ public class LinkedListPractice {
 
     public void Display(){
         if(head==null) System.out.println("the list is empty!");
+
         else{
             Data temp=head;
             while(temp!= null){
@@ -40,12 +44,25 @@ public class LinkedListPractice {
             System.out.println("NULL");
         }
     }
+    public void middle(){
+        if(head==null) System.out.println("empty list");
+        if(head.next==null) System.out.println("the list has only one node");
+        Data slow = head,fast=head;
+        while(fast!=null&&fast.next!=null){
+
+                fast=fast.next.next;
+
+            slow=slow.next;
+        }
+        System.out.println("the middle element is : "+slow.data);
+    }
 
 
     public void delete(){
         System.out.print("enter element you want to delete: ");
         int target=sc.nextInt();
         Data temp=head,prev=null;
+
         if(head==null){
             System.out.println("Empty List!");
         }
@@ -59,17 +76,39 @@ public class LinkedListPractice {
         }
 
 
+
     }
-
-
-
 
     public static void main(String[]args){
         LinkedListPractice list = new LinkedListPractice();
-        list.add(20);list.add(50);list.add(30);list.add(40);list.add(60);
-        list.Display();
-        list.delete();
-        list.Display();
+        Scanner sc = new Scanner(System.in);
+    list.add(20);list.add(30);list.add(40);list.add(50);list.add(60);
+list.middle();
+//        System.out.println("WELCOME   1 to ADD | 2 to Display | 3 to Delete | 4 to Exit");
+//        while(true){
+//            System.out.print("Enter your choice: ");
+//            int  choice= sc.nextInt();
+//            switch(choice){
+//                case 1:
+//                    System.out.print("Enter the element: ");
+//                    int value=sc.nextInt();
+//                    list.add(value);
+//                    break;
+//                case 2:
+//                    list.Display();
+//                    break;
+//                case 3:
+//                    list.delete();
+//
+//                    break;
+//                case 4:
+//                    return;
+//
+//                default:
+//                    System.out.println("Enter the valid number");
+//            }
+       // }
+
 
     }
 }
--- src/contactManagementWithDB/DBConnection.java
+++ src/contactManagementWithDB/DBConnection.java
@@ -1,23 +1,28 @@
-package contactManagementWithDB;
-import java.sql.Connection;
-import java.sql.DriverManager;
-public class DBConnection {
-    private static final String URL = "jdbc:mysql://localhost:3306/ContactManager";
-    private static final String USER = "root";
-    private static final String PASSWORD = "*****8"; ////ACTUAL PASSWORD HERE
-    public static Connection getConnection(){
-        try{
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            return DriverManager.getConnection(URL, USER, PASSWORD);
-        }catch(Exception e){
-            System.out.println("Database connection failed: " + e.getMessage());
-            return null;
-        }
-    }
-    public static void main(String[]args){
-        Connection conn = getConnection();
-        if(conn != null){
-            System.out.println("✅ Connected to MySQL successfully!");
-        }
-    }
-}
+//package contactManagementWithDB;
+//import java.sql.Connection;
+//import java.sql.DriverManager;
+//public class DBConnection {
+//    private static final String URL = "jdbc:mysql://localhost:3306/ContactManager";
+//    private static final String USER = "root";
+//    private static final String PASSWORD = "******"; ////ACTUAL PASSWORD HERE
+//    public static Connection getConnection(){
+//        try{
+//            Class.forName("com.mysql.cj.jdbc.Driver");
+//            return DriverManager.getConnection(URL, USER, PASSWORD);
+//        }catch(Exception e){
+//            System.out.println("Database connection failed: " + e.getMessage());
+//            return null;
+//        }
+//    }
+//    public static void main(String[]args){
+//        Connection conn = getConnection();
+//        if(conn != null){
+//            System.out.println("✅ Connected to MySQL successfully!");
+//        }
+//    }
+//}
+
+
+
+
+
